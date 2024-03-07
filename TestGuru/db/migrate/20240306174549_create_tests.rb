f84d98aa1d0c9ebea4f76dfe6ba47a5c12d76397
Fedor Chesnokov <fedor@chesnokov-ror-course.cloud.lab>
2024-03-07 13:10:31 +0300

    add lesson_2

---
+++ TestGuru/db/migrate/20240306174549_create_tests.rb
@@ -0,0 +1,9 @@
+class CreateTests < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tests do |t|
+      t.string :title
+      t.integer :level, default: 0
+      t.timestamps
+    end
+  end
+end
