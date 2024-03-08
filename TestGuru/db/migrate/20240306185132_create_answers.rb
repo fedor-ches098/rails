21ad8bb696fc54e0d6b05c34fe3982c1b53d4947
Fedor Chesnokov <fedor@chesnokov-ror-course.cloud.lab>
2024-03-08 10:36:58 +0300

    edit lesson_2

--- TestGuru/db/migrate/20240306185132_create_answers.rb
+++ TestGuru/db/migrate/20240306185132_create_answers.rb
@@ -2,8 +2,8 @@ class CreateAnswers < ActiveRecord::Migration[6.1]
   def change
     create_table :answers do |t|
       t.text :body
-      t.boolean :correct
-
+      t.boolean :correct, default: false
+      t.references :question, foreign_key: true
       t.timestamps
     end
   end
--- TestGuru/db/migrate/20240307100516_add_answers_correct_dafault.rb
+++
@@ -1,5 +0,0 @@
-class AddAnswersCorrectDafault < ActiveRecord::Migration[6.1]
-  def change
-    change_column_default(:answers, :correct, false)
-  end
-end
