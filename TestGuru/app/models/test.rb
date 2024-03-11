44773983f8f6ff6079f81119897a4af7fd163a8b
Fedor Chesnokov <fedor@chesnokov-ror-course.cloud.lab>
2024-03-11 16:43:20 +0300

    add lesson3

--- TestGuru/app/models/test.rb
+++ TestGuru/app/models/test.rb
@@ -1,2 +1,8 @@
 class Test < ApplicationRecord
+  belongs_to :user
+  belongs_to :category
+
+  def self.sort_by_category(category_name)
+    joins(:category).where("categories.title = ?", category_name).order(title: :desc).pluck(:title)
+  end
 end
--- TestGuru/db/seeds.rb
+++ TestGuru/db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+categories = Category.create!([
+    {title: "Ruby"},
+    {title: "Linux"}
+])
+
+users = User.create!([
+    {name: "Fedor"},
+    {name: "Masha"},
+    {name: "Olga"}
+])
+
+answers = Answer.create!([
+    {body: "puts", correct: true},
+    {body: "get", correct: false},
+    {body: "cd", correct: true},
+    {body: "pwd", correct: false},
+    {body: "hash", correct: true},
+    {body: "array", correct: false},
+    {body: "mkdir", correct: true},
+    {body: "mk", correct: false}
+])
+
+questions = Question.create!([
+    {body: "A command in Linux to change directory?"},
+    {body: "Method in Ruby that is used to display the output of a program on the console?"},
+    {body: "Dictionary-like collection of unique keys and their values in Ruby?"},
+    {body: "A command-line utility that allows users to create new directories in Linux?"}
+])
+
+tests = Test.create!([
+    {title: "Ruby for beginners", level: 1, category_id: categories[0].id, user_id: users[0].id},
+    {title: "Linux for beginners", level: 1, category_id: categories[1].id, user_id: users[1].id}
+])
