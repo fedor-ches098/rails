e03405e9978c0e325623f1f9354fa0ec57ff70c1
Fedor Chesnokov <fedor@chesnokov-ror-course.cloud.lab>
2024-03-15 12:34:10 +0300

    edit lesson_4

--- TestGuru/db/migrate/20240306174549_create_tests.rb
+++ TestGuru/db/migrate/20240306174549_create_tests.rb
@@ -4,7 +4,7 @@ class CreateTests < ActiveRecord::Migration[6.1]
       t.string :title, null: false
       t.integer :level, default: 0
       t.references :category, foreign_key: true
-      t.references :author, foreign_key: {to_table: :users}
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
