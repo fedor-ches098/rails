38935f1e7dfcd9e09e188337f8150558ad9424da
Fedor Chesnokov <fedor@chesnokov-ror-course.cloud.lab>
2024-03-19 11:55:36 +0300

    add lesson_5

--- TestGuru/app/models/test.rb
+++ TestGuru/app/models/test.rb
@@ -7,7 +7,19 @@ class Test < ApplicationRecord
   has_many :test_passages
   has_many :users, through: :test_passages
 
+  scope :easy, -> {where(level: 0..1)}
+  scope :medium, -> {where(level: 2..4)}
+  scope :hard, -> {where(level: 5..Float::INFINITY)}
+
+  scope :by_category, ->(category_name) {
+    joins(:category).where("categories.title = ?", category_name)
+  }
+  
+  validates :title, presence: true, uniqueness: { scope: :level, message: "should be uniq"}
+                                  
+  validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def self.sort_by_category(category_name)
-    joins(:category).where("categories.title = ?", category_name).order(title: :desc).pluck(:title)
+    by_category.order(title: :desc).pluck(:title)
   end
 end
--- TestGuru/db/schema.rb
+++ TestGuru/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_03_15_092018) do
+ActiveRecord::Schema.define(version: 2024_03_18_194237) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
@@ -59,6 +59,8 @@ ActiveRecord::Schema.define(version: 2024_03_15_092018) do
     t.text "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "answers", "questions"
