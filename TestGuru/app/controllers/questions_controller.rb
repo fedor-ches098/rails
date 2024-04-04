2decaa80335de88b8277d799fe12b91eba0eef6e
fedor-ches098 <fedor@chesnokov-ror-course.cloud.lab>
2024-04-04 12:54:25 +0300

    edit lesson_7

--- TestGuru/app/controllers/questions_controller.rb
+++ TestGuru/app/controllers/questions_controller.rb
@@ -1,12 +1,8 @@
 class QuestionsController < ApplicationController
-  before_action :find_test, only: [:new, :index, :create]
+  before_action :find_test, only: [:new, :create]
   before_action :find_question, only: [:show, :edit, :update, :destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
-
-  def index
-    @questions = @test.questions
-  end
 
   def show
   end
