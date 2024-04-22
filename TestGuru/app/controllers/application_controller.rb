64dab018fc28967a5a30f333e5d562852ecdb9c2
fedor-ches098 <fedor@chesnokov-ror-course.cloud.lab>
2024-04-22 11:18:02 +0300

    add lesson_10

--- TestGuru/app/controllers/application_controller.rb
+++ TestGuru/app/controllers/application_controller.rb
@@ -1,23 +1,16 @@
 class ApplicationController < ActionController::Base
-
+  
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  helper_method :current_user, :logged_in?
-                 
-  private
+  protected
 
-  def authenticate_user!
-    unless current_user
-      cookies[:before_url] = request.url
-      redirect_to root_path, alert: 'Are you a Guru? Verify your Email and Password please'
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
 
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-  end
-
-  def logged_in?
-    @current_user.present?
+  def after_sign_in_path_for(resource)
+    flash[:notice] = "Hello, #{current_user.first_name}!"
+    resource.is_a?(Admin) ? admin_tests_path : tests_path
   end
 end
