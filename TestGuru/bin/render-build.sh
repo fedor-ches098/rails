768f590b3a425bcac75148c8089e0640e3e396a1
fedor-ches098 <fedor@chesnokov-ror-course.cloud.lab>
2024-05-17 13:37:04 +0300

    edit lesson_16

--- TestGuru/bin/render-build.sh
+++ TestGuru/bin/render-build.sh
@@ -5,3 +5,6 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+bundle exec rake db:schema:dump
+bundle exec rake rake db:schema:load RAILS_ENV=production
+bundle exec rails db:migrate RAILS_ENV=production 
