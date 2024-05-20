758f60c04bcd24f6c79829b694d7ca712833f457
fedor-ches098 <fedor@chesnokov-ror-course.cloud.lab>
2024-05-20 17:43:27 +0300

    edit lesson_16

--- TestGuru/app/models/question.rb
+++ TestGuru/app/models/question.rb
@@ -3,6 +3,7 @@ class Question < ApplicationRecord
 
   has_many :answers, dependent: :destroy
   has_many :gists, dependent: :destroy
+  has_one :test_passages, foreign_key: :current_question_id, dependent: :destroy, class_name: 'TestPassage' 
 
   validates :body, presence: true
 end
